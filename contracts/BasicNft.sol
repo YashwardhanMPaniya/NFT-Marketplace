ece9059dabaaf715374a4c4a901899eb45f30d22
root <root@LAPTOP-CPJ6OJI0>
2024-08-11 15:29:46 +0530

    code change

--- contracts/BasicNft.sol
+++ contracts/BasicNft.sol
@@ -1,13 +1,12 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.7;
+pragma solidity ^0.8.8;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract BasicNftTwo is ERC721 {
-    string public constant TOKEN_URI = "ipfs://QmdryoExpgEQQQgJPoruwGJyZmz6SqV4FRTX1i73CT3iXn";
+contract BasicNft is ERC721 {
+    string public constant TOKEN_URI =
+        "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
     uint256 private s_tokenCounter;
-
-    event DogMinted(uint256 indexed tokenId);
 
     constructor() ERC721("Dogie", "DOG") {
         s_tokenCounter = 0;
@@ -15,12 +14,11 @@ contract BasicNftTwo is ERC721 {
 
     function mintNft() public {
         _safeMint(msg.sender, s_tokenCounter);
-        emit DogMinted(s_tokenCounter);
         s_tokenCounter = s_tokenCounter + 1;
     }
 
     function tokenURI(uint256 tokenId) public view override returns (string memory) {
-        require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
+        // require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
         return TOKEN_URI;
     }
 
